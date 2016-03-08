e71ec52a32c1c3b1616864854d927b606d8cdbcd
figrrygg <gregers.gram.rygg@finn.no>
2016-03-08 12:33:35 +0100

    Add env argument to heroku script

--- infrastructure/heroku.js
+++ infrastructure/heroku.js
@@ -2,12 +2,23 @@ const heroin = require('heroin-js');
 const PROD = require('./prod.js');
 const TEST = require('./test.js');
 
+const env = process.argv[2];
+
 const configurator = heroin(process.env.HEROKU_API_TOKEN, {
     debug: false
 });
 
-configurator(PROD);
-configurator(TEST);
+if (env === 'prod') {
+    console.log('prod');
+    configurator(PROD);
+} else if (env === 'test') {
+    console.log('test');
+    configurator(TEST);
+} else {
+    console.error('No env specified');
+    process.exit(1);
+}
+
 
 /*configurator.export('book-inventory-service-gregers').then(function(result) {
     console.log(result);
