6be59f02c4891b290da84e8853e2b2b244fca22b
figrrygg <gregers.gram.rygg@finn.no>
2016-03-09 10:52:17 +0100

    Only use log_drain in prod

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -1,2 +1,6 @@
+/*eslint camelcase: 0*/
+
 const BASECONF = require('./baseconfig.js');
-module.exports = Object.assign({}, BASECONF);
+module.exports = Object.assign({
+    log_drains: ['syslog://data.logentries.com:13636']
+}, BASECONF);
