15650092587f781b80b0ce7bd954d58787682b6f
figrrygg <gregers.gram.rygg@finn.no>
2016-03-09 10:50:08 +0100

    Add log_drain

--- infrastructure/baseconfig.js
+++ infrastructure/baseconfig.js
@@ -39,6 +39,6 @@ module.exports = {
         quantity: 1,
         size: 'Free'
     }],
-    log_drains: []/*,
+    log_drains: ['syslog://data.logentries.com:13636']/*,
     domains: ['book-inventory-service-gregers.herokuapp.com']*/
 };
